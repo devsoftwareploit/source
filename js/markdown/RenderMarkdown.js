2822805878a94e34ed02ae9c02123c35f7e24b13
DEV SOFTWARE PLOIT <softwareploitoken@gmail.com>
2024-08-15 18:52:39 -0700

    Create RenderMarkdown.js

---
+++ js/markdown/RenderMarkdown.js
@@ -0,0 +1,6 @@
+
+function render() {
+    var userText = document.getElementById("textarea").value;
+    document.getElementById("render").innerHTML = marked.parse(userText);
+        localStorage.setItem("md", userText);
+}
